0f49d04b68221d40afe8ecd0ff5a897e4e77af8e
sandeep <sandeep@ashmar.in>
2019-09-23 15:21:49 +0530

    add

---
+++ app/src/main/java/com/example/mysdk/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mysdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.secondlib.Addition;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Addition ref=new Addition();
+//       int add= ref.sum(4,5);
+//      Toast.makeText(this, ""+add, Toast.LENGTH_LONG).show();
+
+    }
+}
---
+++ secondlib/src/main/java/com/example/secondlib/Addition.java
@@ -0,0 +1,9 @@
+package com.example.secondlib;
+
+public class Addition {
+
+    public int sum( int x,int y){
+
+        return x+y;
+    }
+}
